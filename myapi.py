71b9be65014902edad0c5781005ba6fa85c71239
NorIslam04 <islamaoudi18@gmail.com>
2024-12-15 08:18:36 +0100

    add post methode with quety in the path + optionel parametre

--- myapi.py
+++ myapi.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,Path
-
+from typing import Optional
 app = FastAPI()
 
 data={
@@ -18,6 +18,18 @@ data={
 def read_root():#function to be executed when root path is accessed
     return {"message": "Hello World"}#json format response
 
-@app.get("/data/{item_id}")#path with parameter item_id 
+#path with parameter item_id 
+#use: /data/1
+@app.get("/data/{item_id}")
 def read_data(item_id: int = Path(...,description="enter the ID of user!",gt=0)):#function to be executed when path is accessed
     return data[item_id]#return data with key as item_id
+
+#path with query parameter, item_id is optional
+#use: /data/?item_id=1 or /data/
+@app.get("/data/")
+#def get_with_name(name: Optional[str] =None,test : int): #this will give error because of test parameter is not optional we must first define non optional parameters then optional parameters
+def get_with_name(test : int ,name: Optional[str]=None):
+    for key in data:
+        if data[key]["name"].lower()==name.lower():
+            return data[key]
+    return {"error":"Data not found!"}
